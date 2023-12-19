b6a7d5897c4d5b3e6fa214c9a174039e4010d7d5
NeiltonSeguins <seguins.neilton@gmailcom>
2023-12-19 15:48:07 -0300

    Vídeo 1.3 - Componente Radio Button

---
+++ src/componentes/BotaoRadio/BotaoRadioGrupo.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface BotaoRadioGrupoProps {
+  children: React.ReactNode;
+  legenda?: string;
+  ariaLabel: string;
+}
+
+const BotaoRadioGrupo = ({
+  children,
+  legenda,
+  ariaLabel,
+}: BotaoRadioGrupoProps) => {
+  return (
+    <fieldset
+      className="radio__grupo--campos"
+      aria-label={ariaLabel}
+      tabIndex={0}
+    >
+      {legenda && <legend className="radio__grupo--legenda">{legenda}</legend>}
+      <ul className="radio__grupo--campos">{children}</ul>
+    </fieldset>
+  );
+};
+
+export default BotaoRadioGrupo;
