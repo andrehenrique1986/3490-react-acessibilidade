61224636b86d4caf59544fc99667d46e64f9c1b3
NeiltonSeguins <seguins.neilton@gmailcom>
2023-12-20 15:13:45 -0300

    Vídeo 2.1 - Criando o Acordeão

---
+++ src/componentes/Acordeao/AcordeaoConteudo.tsx
@@ -0,0 +1,9 @@
+interface AcordeaoConteudoProps {
+  children: React.ReactNode;
+}
+
+const AcordeaoConteudo = ({ children }: AcordeaoConteudoProps) => {
+  return <details className="acordeao__conteudo">{children}</details>;
+};
+
+export default AcordeaoConteudo;
