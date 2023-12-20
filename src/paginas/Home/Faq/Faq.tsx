ddf2649cc5c2ec7ce480e98a4bf89b901d45fdbb
NeiltonSeguins <seguins.neilton@gmailcom>
2023-12-20 15:40:28 -0300

    Vídeo 2.3 - Usando o componente de Acordeão

--- src/paginas/Home/Faq/Faq.tsx
+++ src/paginas/Home/Faq/Faq.tsx
@@ -1,8 +1,12 @@
 import "./Faq.css";
 import Tipografia from "@/componentes/Tipografia";
+import faq from "../../../assets/faq.json";
+import Acordeao from "@/componentes/Acordeao/Acordeao";
+import AcordeaoConteudo from "@/componentes/Acordeao/AcordeaoConteudo";
+import AcordeaoTitulo from "@/componentes/Acordeao/AcordeaoTitulo";
+import AcordeaoDescricao from "@/componentes/Acordeao/AcordeaoDescricao";
 
 const Faq = () => {
-
   return (
     <section className="secao__duvidas">
       <Tipografia elemento="h2" variante="h2" cor="azul">
@@ -12,6 +16,14 @@ const Faq = () => {
         Antes de entrar em contato, verifique se sua dúvida está respondida em
         nossa FAQ!
       </Tipografia>
+      <Acordeao>
+        {faq.map((duvida) => (
+          <AcordeaoConteudo key={duvida.id}>
+            <AcordeaoTitulo titulo={duvida.titulo} id={duvida.id} />
+            <AcordeaoDescricao descricao={duvida.descricao} id={duvida.id} />
+          </AcordeaoConteudo>
+        ))}
+      </Acordeao>
     </section>
   );
 };
